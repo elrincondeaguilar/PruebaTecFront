c536c1afdeded6cfbb9a59da5ed75b45b3d03fdf
jfar1988 <jfar1988@misena.edu.co>
2025-03-13 21:47:54 -0500

    style: cambiar clase 'animate-bounce text-center' en users y posts

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -6,6 +6,7 @@ import { fetchPosts } from "@/utils/fetchPosts";
 import { useQuery } from "@tanstack/react-query";
 import { motion, AnimatePresence } from "framer-motion";
 import Skeleton from "@/components/Skeleton";
+import { h1 } from "framer-motion/client";
 
 export default function PostDetail({ params }: { params: { id: string } }) {
   const {
@@ -55,6 +56,11 @@ export default function PostDetail({ params }: { params: { id: string } }) {
   if (isLoading) {
     return (
       <div className="max-w-2xl mx-auto p-6">
+        <div className="animate-bounce text-center">
+          <h1 className="text-3xl font-bold text-center mb-6">
+            Cargando publicación...
+          </h1>
+        </div>
         <Skeleton width="80%" height="30px" />
         <Skeleton width="100%" height="20px" />
         <Skeleton width="100%" height="20px" />
--- src/app/users/[id]/page.tsx
+++ src/app/users/[id]/page.tsx
@@ -22,9 +22,9 @@ export default function UserDetail({ params }: { params: { id: string } }) {
   if (isLoading) {
     return (
       <div className="max-w-2xl mx-auto p-6">
-        <h1 className="text-3xl font-bold text-center mb-6">
-          Cargando usuario...
-        </h1>
+        <div className="animate-bounce text-center">
+          <h1 className="text-3xl font-bold mb-6">Cargando usuario...</h1>
+        </div>
         <Skeleton width="80%" height="30px" />
         <Skeleton width="100%" height="20px" />
         <Skeleton width="100%" height="20px" />
